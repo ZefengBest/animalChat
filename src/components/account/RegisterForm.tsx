039195d8b1c6701040c005580b6001edb4b3c814
ZefengBest <nzf2000316@163.com>
2023-12-02 19:45:02 +0800

    Register with username and pwd, set db connection

--- src/components/account/RegisterForm.tsx
+++ src/components/account/RegisterForm.tsx
@@ -1,35 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const RegisterForm = () => {
   const navigate = useNavigate()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [confirmPasswordError, setConfirmPasswordError] = useState('')
+  const [passwordsMatch, setPasswordsMatch] = useState(true)
 
-  const handleRegister = () => {
-    navigate('/animal-selection')
+  async function onSubmit(e: { preventDefault: () => void }) {
+    e.preventDefault()
+    if (validatePassword()) {
+      console.log(username + ' ' + password)
+      axios
+        .post('http://localhost:5001/record/add', {
+          username,
+          password
+        })
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+      console.log('User added successfully')
+      navigate('/')
+    }
+  }
+
+  const validatePassword = () => {
+    let isValid = false
+    const password = document.getElementById('password') as HTMLInputElement
+    const confirmPassword = document.getElementById(
+      'confirmPassword'
+    ) as HTMLInputElement
+    if (password.value !== confirmPassword.value) {
+      //display a line of alert
+      setConfirmPasswordError('Passwords do not match')
+      setPasswordsMatch(false)
+      confirmPassword.setCustomValidity('Passwords do not match')
+    } else {
+      isValid = true
+      setPasswordsMatch(true)
+      confirmPassword.setCustomValidity('')
+    }
+    //add log to print isValid value
+    console.log(isValid)
+    return isValid
   }
 
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="confirmPassword">Confirm Password</label>
-          <input type="password" id="confirmPassword" />
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value)
+              setPasswordsMatch(true) // Reset password matching state on each change
+            }}
+          />
         </div>
+        {!passwordsMatch && (
+          <div style={{ color: 'red', marginBottom: '10px' }}>
+            Passwords do not match
+          </div>
+        )}
         <div>
           <label htmlFor="rememberMe">Remember Me</label>
           <input type="checkbox" id="rememberMe" />
         </div>
-        <button type="button" onClick={handleRegister}>
-          Register
-        </button>
+        <button type="submit">Register</button>
       </form>
     </div>
   )
